db241cdb0b7201934aaa856331e9dacb44e148e3
Shykat Raha <devshykat2022@gmail.com>
2025-12-22 23:36:14 +0600

    Point table champion trophy

--- app/components/landing/Point-table.vue
+++ app/components/landing/Point-table.vue
@@ -52,7 +52,7 @@ const { data, pending } = useAPI("point-table", `/points/2`);
                 :src="d?.team?.club?.logo"
                 class="size-8 object-cover rounded-full"
               />
-              <span>{{ d?.team?.club?.name }}</span>
+              <span>{{ d?.team?.club?.name }} {{ index === 0 && "🏆" }}</span>
             </div>
 
             <div class="px-4 py-2 col-span-2 space-y-1">
