12aa61b57603e60ba3b0dd2b425815a4429a42b9
Shykat Raha <shykat@co.design>
2026-01-08 15:55:13 +0600

    Fix: score position

--- app/components/landing/Header.vue
+++ app/components/landing/Header.vue
@@ -8,14 +8,11 @@ const app = useAppStore();
 <template>
   <header class="flex justify-between items-start">
     <div class="flex items-center gap-4">
-      <img
-        src="/assets/images/fc.jpg"
-        alt="fc"
-        class="size-12 lg:size-16 rounded-lg border border-border object-cover"
-      />
+      <img src="/assets/images/fc.jpg" alt="fc"
+        class="size-12 lg:size-16 rounded-lg border border-border object-cover" />
 
       <div>
-        <h1 class="text-xl lg:text-3xl text-brand font-semibold">
+        <h1 class="text-lg lg:text-3xl text-brand font-semibold">
           FC26 Tournament
         </h1>
         <span>Ongoing</span>
@@ -23,16 +20,11 @@ const app = useAppStore();
     </div>
 
     <div>
-      <USelect
-        v-model="app.matchType"
-        value-key="value"
-        :items="[
-          { label: 'Pre Season', value: 'PRESEASON' },
-          { label: 'Semi Final', value: 'SEMIFINAL' },
-          // { label: 'Final', value: 'FINAL' },
-        ]"
-        class="w-32 md:w-48 py-2"
-        :ui="{
+      <USelect v-model="app.matchType" value-key="value" :items="[
+        { label: 'Pre Season', value: 'PRESEASON' },
+        { label: 'Semi Final', value: 'SEMIFINAL' },
+        // { label: 'Final', value: 'FINAL' },
+      ]" class="w-32 md:w-48 py-2" :ui="{
           base: 'bg-brand/10 text-brand ring-1 ring-brand/30 hover:bg-brand/20 focus:ring-1 focus:ring-brand',
           trailingIcon: 'text-brand',
 
@@ -41,8 +33,7 @@ const app = useAppStore();
           item: 'text-brand px-3 py-2 rounded-md transition hover:bg-card/50 data-highlighted:bg-card',
 
           viewport: 'bg-dark',
-        }"
-      />
+        }" />
     </div>
   </header>
 </template>
